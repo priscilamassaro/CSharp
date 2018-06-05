3752323ac69d02e34f45ce22a462af0bbb2875b0
priscila <priscila.massaro.job@gmail.com>
2018-06-04 22:25:11 -0300

    WPF

---
+++ ControleAtividades/WpfView/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfView
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Eai usuário, tudo bem?");
+        }
+
+        private void checkBox1_Checked(object sender, RoutedEventArgs e)
+        {
+            btnCliqueMe.Visibility = Visibility.Hidden;
+        }
+
+
+        private void checkBox1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            btnCliqueMe.Visibility = Visibility.Visible;
+        }
+
+        private void btnVerCadastro_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroUsuario cadUsu = new CadastroUsuario();
+            //cadUsu.Show();
+            cadUsu.ShowDialog(); //mostra a segunda tela mas bloqueia a anterior
+        }
+    }
+}
